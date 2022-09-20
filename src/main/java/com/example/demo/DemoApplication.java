3e2e2bb20dfdb0ba8da0b19e7ee450edb3526d0e
cyi <cyi@DESKTOP-QMJFGD0>
2022-09-20 11:03:10 +0900

    develop 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("aaa");
+		System.out.println("aaa -> bbb develop 수정");
+		System.out.println("develop 추가");
 	}
 
 }
