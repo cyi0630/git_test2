05226c25b79314fe5f6d0f073a396119f9336b9b
cyi <cyi@DESKTOP-QMJFGD0>
2022-09-20 11:22:41 +0900

    develop 추가 수정 및 222 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,9 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("aaa -> bbb develop 수정");
-		System.out.println("develop 추가");
+		Syst1em.out.println("aaa -> bbb develop 수정");
+		System.out.println("develop 추가 - master에서는 삭제했는데 develop에서 수정함");
+		Syst1em.out.println("develop 추가222");
 	}
 
 }
