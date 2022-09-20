d12fcb194b351fe25c8735acc43ef891b66253bf
cyi <cyi@DESKTOP-QMJFGD0>
2022-09-20 13:36:18 +0900

    ㄹㄹㄹ

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ public class DemoApplication {
 		Syst1em.out.println("aaa -> bbb develop 수정");
 		System.out.println("develop 추가 - master에서는 삭제했는데 develop에서 수정함");
 		Syst1em.out.println("develop 추가222");
+		Syst1em.out.println("develop 추가333");
 		//master
 		System.out.println("aaa -> bbb develop 수정");
 		System.out.println("master 추가");
